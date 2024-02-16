892ce655e36358a7e01528ab0a1ede9a2c520a86
Abdo <abdessamadelfathi2018@gmail.com>
2024-02-16 17:06:44 +0100

    first commit

---
+++ src/services/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from "@supabase/supabase-js";
+const supabaseUrl = "https://eozkelofukfuaanlfbdb.supabase.co";
+const supabaseKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVvemtlbG9mdWtmdWFhbmxmYmRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwMjU3MTYsImV4cCI6MjAyMzYwMTcxNn0.AWYTtSic8JgU7P5pBY8apT9EXFyPqfTr8J9uQN1_J2g";
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
